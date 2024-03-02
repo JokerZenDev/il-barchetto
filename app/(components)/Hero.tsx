8a667278a58e47b0fa6b7213f12544dbc0062333
JokerZenDev <michele.lenzi98@gmail.com>
2024-03-02 23:14:32 +0100

    feat(hero): add param to change size image

--- app/(components)/Hero.tsx
+++ app/(components)/Hero.tsx
@@ -11,24 +11,28 @@ export default function Hero({
 	title,
 	img,
 	subtitle,
+	size
 }: {
 	surtitle?: string
 	title: string
-	img: string
+	img?: string
 	subtitle?: string
+	size: "sm" | "lg"
 }) {
 	return (
 		<div
-			className={`relative flex m-0 h-[90vh] max-h-[1000px] items-center justify-center ${satisfy.className}`}
+			className={`relative flex m-0 ${size === "lg" ? "h-[90vh]" : "h-[50vh]"} max-h-[1000px] items-center justify-center ${satisfy.className}`}
 		>
-			<Image
-				src={img}
-				alt={title}
-				fill={true}
-				objectFit='cover'
-				priority
-				className='z-0'
-			/>
+			{img && (
+				<Image
+					src={img}
+					alt={title}
+					fill={true}
+					objectFit='cover'
+					priority
+					className='z-0'
+				/>
+			)}
 			<div className='text-white mx-4 lg:mx-44 w-full flex flex-col gap-1 lg:gap-2 z-[1]'>
 				{surtitle && (
 					<span className='text-3xl lg:text-4xl font-thin'>
--- app/(components)/Main.tsx
+++ app/(components)/Main.tsx
@@ -7,16 +7,18 @@ export default function Main({
 	title,
 	img,
 	subtitle,
+	size = "lg",
 }: {
 	children: React.ReactNode
 	surtitle?: string
 	title: string
-	img: string
+	img?: string
 	subtitle?: string
+	size?: "sm" | "lg"
 }) {
 	return (
 		<main>
-			<Hero surtitle={surtitle} title={title} img={img} />
+			<Hero surtitle={surtitle} title={title} img={img} size={size} />
 			<Content title={subtitle}>{children}</Content>
 		</main>
 	)
