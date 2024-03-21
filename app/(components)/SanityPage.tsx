650167efac0e494f6c60c6e9fc8301b67af2dceb
JokerZenDev <michele.lenzi98@gmail.com>
2024-03-21 14:14:00 +0100

    feat(b&b): add prices accomodation

--- app/(components)/SanityPage.tsx
+++ app/(components)/SanityPage.tsx
@@ -9,11 +9,17 @@ export default function SanityPage({
 	dict,
 	lang,
 	data,
+	table,
 	footnotes,
 }: {
 	dict: { [key: string]: { [key: string]: string | string[] } }
 	lang: string
 	data: Page
+	table?: {
+		header: string[]
+		rows: string[][]
+		footer: string[]
+	}
 	footnotes?: string
 }) {
 	return (
@@ -29,6 +35,31 @@ export default function SanityPage({
 				{data.blockContent && (
 					<div className='flex flex-col gap-4 text-justify'>
 						<MyPortableText value={data.blockContent} />
+					</div>
+				)}
+				{table && (
+					<div className='w-full flex flex-col items-start gap-4'>
+						<table className='w-full max-w-3xl mx-auto'>
+							<thead>
+								<tr className="border-b">
+									<th className="p-2">{table.header[0]}</th>
+									<th className="p-2">{table.header[1]}</th>
+								</tr>
+							</thead>
+							<tbody>
+								{table.rows.map((item, key) => (
+									<tr key={key} className="border-b">
+										<td className="p-2">{item[0]}</td>
+										<td className="p-2">{item[1]}</td>
+									</tr>
+								))}
+							</tbody>
+						</table>
+						<ul className="list-disc flex flex-col gap-1 ml-4">
+							{table.footer.map((item, key) => (
+								<li key={key}>{item}</li>
+							))}
+						</ul>
 					</div>
 				)}
 				{data.content && (
