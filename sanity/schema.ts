89efea08e98482276d6faa6149eda27ddc262b5b
JokerZenDev <michele.lenzi98@gmail.com>
2024-03-25 11:19:08 +0100

    fix(*): remove old component from sanity schema

--- sanity/schema.ts
+++ sanity/schema.ts
@@ -5,7 +5,6 @@ import hero from './schemas/hero'
 import gallery from './schemas/gallery'
 import menu from './schemas/menu'
 import content from './schemas/content'
-import newPage from './schemas/newPage'
 
 export const schema: { types: SchemaTypeDefinition[] } = {
   types: [page, hero, gallery, blockContent, content, menu],
